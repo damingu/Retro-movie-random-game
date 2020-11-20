9636e0db2d5911d5fe6062c1443b58ffe9b2a259
wealways <likeletter5658@gmail.com>
2020-11-20 20:24:25 +0900

    Add like_user field in MovieModel

--- game/models.py
+++ game/models.py
@@ -15,3 +15,4 @@ class Movie(models.Model):
     overview = models.TextField(null=True)
     poster_path = models.CharField(null=True,max_length=500)
     genres = models.ManyToManyField(Genre)
+    like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
